039ee534108cf056660968a7a067502ac5650dec
Tobias Schramm <t.schramm@manjaro.org>
2021-05-29 14:41:08 +0200

    permit storage of non-contiguous objects

--- objectlog.h
+++ objectlog.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include "ringbuffer.h"
 
@@ -13,9 +14,15 @@ typedef struct {
 
 typedef long objectlog_iterator_t;
 
+typedef struct {
+	const void *ptr;
+	uint16_t len;
+} scatter_object_t;
+
 void objectlog_init(objectlog_t *log, void *storage, uint16_t size);
-void objectlog_write_object(objectlog_t *log, const void *data, uint16_t len);
-void objectlog_write_string(objectlog_t *log, const char *str);
+uint16_t objectlog_write_object(objectlog_t *log, const void *data, uint16_t len);
+uint16_t objectlog_write_scattered_object(objectlog_t *log, const scatter_object_t *scatter_list);
+uint16_t objectlog_write_string(objectlog_t *log, const char *str);
 objectlog_iterator_t objectlog_iterator(objectlog_t *log, int object_idx);
 const void *objectlog_get_fragment(objectlog_t *log, objectlog_iterator_t iterator, uint8_t *len);
 objectlog_iterator_t objectlog_next(objectlog_t *log, objectlog_iterator_t iterator);
