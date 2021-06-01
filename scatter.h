86e77a53529585bf1a33259c6b20d49f3f622dde
Tobias Schramm <t.schramm@manjaro.org>
2021-06-02 01:23:44 +0200

    Fix scatter header

--- scatter.h
+++ scatter.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdint.h>
 
 typedef struct {
@@ -5,7 +7,7 @@ typedef struct {
 	uint16_t len;
 } scatter_object_t;
 
-static inline uint16_t scatter_list_size(scatter_object_t *sc_list) {
+static inline uint16_t scatter_list_size(const scatter_object_t *sc_list) {
 	uint16_t len = 0;
 
 	while(sc_list->len) {
