1a2984732947adf267ad7768032025daf81c1d54
Tobias Schramm <t.schramm@manjaro.org>
2021-06-04 14:54:56 +0200

    Move larger functions into separate compilation unit

---
+++ multiring.c
@@ -0,0 +1,186 @@
+#include <string.h>
+
+#include "multiring.h"
+
+int multiring_init(multiring_t *multiring, const scatter_object_t *storage) {
+	const scatter_object_t *sc_entry = storage;
+	unsigned int sc_max_entry_idx = 0;
+	scatter_object_t *sc_list_copy;
+	unsigned int num_storage_area = 0;
+	uint16_t storage_list_size;
+	uint16_t storage_size = 0;
+
+	while (sc_entry->len) {
+		if (sc_entry->len > storage[sc_max_entry_idx].len) {
+			sc_max_entry_idx = num_storage_area;
+		}
+		storage_size += sc_entry->len;
+		num_storage_area++;
+		sc_entry++;
+	}
+	if (!num_storage_area) {
+		return -1;
+	}
+
+	multiring->num_storage = num_storage_area;
+	storage_list_size = (num_storage_area + 1) * sizeof(scatter_object_t);
+
+	if (storage[sc_max_entry_idx].len < storage_list_size) {
+		return -1;
+	}
+
+	sc_list_copy = storage[sc_max_entry_idx].ptr;
+	memcpy(sc_list_copy, storage, storage_list_size);
+	sc_list_copy[sc_max_entry_idx].ptr =
+		((uint8_t*)sc_list_copy[sc_max_entry_idx].ptr) + storage_list_size;
+	sc_list_copy[sc_max_entry_idx].len -= storage_list_size;
+	storage_size -= storage_list_size;
+	multiring->storage = sc_list_copy;
+	multiring->size = storage_size;
+
+	multiring->ptr_read.storage = sc_list_copy;
+	multiring->ptr_read.offset = 0;
+	multiring->ptr_write.storage = sc_list_copy;
+	multiring->ptr_write.offset = 0;
+	return 0;
+}
+
+void multiring_next_ring(multiring_t *multiring, multiring_ptr_t *ptr) {
+	const scatter_object_t *storage = ptr->storage;
+
+	storage++;
+	if (!storage->len) {
+		storage = multiring->storage;
+	}
+	ptr->storage = storage;
+	ptr->offset = 0;
+}
+
+void multiring_advance(multiring_t *multiring, multiring_ptr_t *ptr,
+		       uint16_t count) {
+	multiring_ptr_t ring_ptr = *ptr;
+
+	while (count) {
+		uint16_t space_ring =
+			multiring_available_contiguous(&ring_ptr);
+
+		if (space_ring > count) {
+			ring_ptr.offset += count;
+			break;
+		}
+		count -= space_ring;
+		multiring_next_ring(multiring, &ring_ptr);
+	}
+
+	*ptr = ring_ptr;
+}
+
+void multiring_write(multiring_t *multiring, const void *data, uint16_t len) {
+	const uint8_t *data8 = data;
+
+	while (len) {
+		uint16_t write_size = len;
+		uint16_t space_avail =
+			multiring_available_contiguous(&multiring->ptr_write);
+
+		if (write_size > space_avail) {
+			write_size = space_avail;
+		}
+
+		memcpy(((uint8_t*)multiring->ptr_write.storage->ptr) + multiring->ptr_write.offset,
+		       data8, write_size);
+		multiring_advance_write(multiring, write_size);
+
+		len -= write_size;
+		data8 += write_size;
+	}
+}
+
+void multiring_read(multiring_t *multiring, void *data, uint16_t len) {
+	uint8_t *data8 = data;
+
+	while (len) {
+		uint16_t read_size = len;
+		uint16_t space_avail =
+			multiring_available_contiguous(&multiring->ptr_read);
+
+		if (read_size > space_avail) {
+			read_size = space_avail;
+		}
+
+		memcpy(data8,
+		       ((uint8_t*)multiring->ptr_read.storage->ptr) + multiring->ptr_read.offset,
+		       read_size);
+		multiring_advance_read(multiring, read_size);
+
+		len -= read_size;
+		data8 += read_size;
+	}
+}
+
+uint16_t multiring_num_wraps(multiring_t *multiring, uint16_t len) {
+	uint16_t num_wraps = 0;
+	multiring_ptr_t ptr = multiring->ptr_write;
+
+	while (len) {
+		uint16_t chunk_size = len;
+		uint16_t space_avail = multiring_available_contiguous(&ptr);
+
+		if (chunk_size > space_avail) {
+			chunk_size = space_avail;
+		}
+
+		if (chunk_size != len) {
+			num_wraps++;
+		}
+
+		multiring_advance(multiring, &ptr, chunk_size);
+		len -= chunk_size;
+	}
+
+	return num_wraps;
+}
+
+void multiring_memset(multiring_t *multiring, uint8_t val, uint16_t len) {
+	while (len) {
+		uint16_t write_size = len;
+		uint16_t space_avail =
+			multiring_available_contiguous(&multiring->ptr_write);
+
+		if (write_size > space_avail) {
+			write_size = space_avail;
+		}
+
+		memset(((uint8_t*)multiring->ptr_write.storage->ptr) + multiring->ptr_write.offset,
+		       val, write_size);
+		multiring_advance_write(multiring, write_size);
+
+		len -= write_size;
+	}
+}
+
+uint16_t multiring_byte_delta(multiring_t *multiring,
+			      multiring_ptr_t *first,
+			      multiring_ptr_t *second) {
+	uint16_t len;
+	multiring_ptr_t ptr = *first;
+
+	/* Simple cases: first and second are from the same scatter list entry */
+	if (first->storage == second->storage) {
+		if (first->offset <= second->offset) {
+			return second->offset - first->offset;
+		}
+		return multiring->size - (first->offset - second->offset);
+	}
+
+	/* Complex case: first and second in different scatter list entries */
+	len = first->storage->len - first->offset;
+	do {
+		multiring_next_ring(multiring, &ptr);
+		len += ptr.storage->len;
+	} while (ptr.storage != second->storage);
+	len -= (second->storage->len - second->offset);
+
+	return len;
+}
+
--- multiring.h
+++ multiring.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "ringbuffer.h"
 #include "scatter.h"
 
 typedef struct {
@@ -16,83 +15,20 @@ typedef struct {
 	uint16_t size;
 } multiring_t;
 
-static inline int multiring_init(multiring_t *multiring,
-				 const scatter_object_t *storage) {
-	const scatter_object_t *sc_entry = storage;
-	unsigned int sc_max_entry_idx = 0;
-	scatter_object_t *sc_list_copy;
-	unsigned int num_storage_area = 0;
-	uint16_t storage_list_size;
-	uint16_t storage_size = 0;
-
-	while (sc_entry->len) {
-		if (sc_entry->len > storage[sc_max_entry_idx].len) {
-			sc_max_entry_idx = num_storage_area;
-		}
-		storage_size += sc_entry->len;
-		num_storage_area++;
-		sc_entry++;
-	}
-	if (!num_storage_area) {
-		return -1;
-	}
-
-	multiring->num_storage = num_storage_area;
-	storage_list_size = (num_storage_area + 1) * sizeof(scatter_object_t);
-
-	if (storage[sc_max_entry_idx].len < storage_list_size) {
-		return -1;
-	}
-
-	sc_list_copy = storage[sc_max_entry_idx].ptr;
-	memcpy(sc_list_copy, storage, storage_list_size);
-	sc_list_copy[sc_max_entry_idx].ptr =
-		((uint8_t*)sc_list_copy[sc_max_entry_idx].ptr) + storage_list_size;
-	sc_list_copy[sc_max_entry_idx].len -= storage_list_size;
-	storage_size -= storage_list_size;
-	multiring->storage = sc_list_copy;
-	multiring->size = storage_size;
-
-	multiring->ptr_read.storage = sc_list_copy;
-	multiring->ptr_read.offset = 0;
-	multiring->ptr_write.storage = sc_list_copy;
-	multiring->ptr_write.offset = 0;
-	return 0;
-}
+int multiring_init(multiring_t *multiring, const scatter_object_t *storage);
+void multiring_next_ring(multiring_t *multiring, multiring_ptr_t *ptr);
+void multiring_advance(multiring_t *multiring, multiring_ptr_t *ptr,
+		       uint16_t count);
+void multiring_write(multiring_t *multiring, const void *data, uint16_t len);
+void multiring_read(multiring_t *multiring, void *data, uint16_t len);
+uint16_t multiring_num_wraps(multiring_t *multiring, uint16_t len);
+void multiring_memset(multiring_t *multiring, uint8_t val, uint16_t len);
+uint16_t multiring_byte_delta(multiring_t *multiring,
+			      multiring_ptr_t *first,
+			      multiring_ptr_t *second);
 
 static inline uint16_t multiring_available_contiguous(const multiring_ptr_t *ptr) {
 	return ptr->storage->len - ptr->offset;
-}
-
-static inline void multiring_next_ring(multiring_t *multiring,
-				       multiring_ptr_t *ptr) {
-	const scatter_object_t *storage = ptr->storage;
-
-	storage++;
-	if (!storage->len) {
-		storage = multiring->storage;
-	}
-	ptr->storage = storage;
-	ptr->offset = 0;
-}
-
-static inline void multiring_advance(multiring_t *multiring, multiring_ptr_t *ptr,
-				     uint16_t count) {
-	multiring_ptr_t ring_ptr = *ptr;
-
-	while (count) {
-		uint16_t space_ring =
-			multiring_available_contiguous(&ring_ptr);
-
-		if (space_ring > count) {
-			ring_ptr.offset += count;
-			break;
-		}
-		count -= space_ring;
-		multiring_next_ring(multiring, &ring_ptr);
-	}
-
-	*ptr = ring_ptr;
 }
 
 static inline void multiring_advance_read(multiring_t *multiring,
@@ -105,49 +41,6 @@ static inline void multiring_advance_write(multiring_t *multiring,
 	multiring_advance(multiring, &multiring->ptr_write, count);
 }
 
-static inline void multiring_write(multiring_t *multiring, const void *data, uint16_t len) {
-	const uint8_t *data8 = data;
-
-	while (len) {
-		uint16_t write_size = len;
-		uint16_t space_avail =
-			multiring_available_contiguous(&multiring->ptr_write);
-
-		if (write_size > space_avail) {
-			write_size = space_avail;
-		}
-
-		memcpy(((uint8_t*)multiring->ptr_write.storage->ptr) + multiring->ptr_write.offset,
-		       data8, write_size);
-		multiring_advance_write(multiring, write_size);
-
-		len -= write_size;
-		data8 += write_size;
-	}
-}
-
-static inline void multiring_read(multiring_t *multiring, void *data, uint16_t len) {
-	uint8_t *data8 = data;
-
-	while (len) {
-		uint16_t read_size = len;
-		uint16_t space_avail =
-			multiring_available_contiguous(&multiring->ptr_read);
-
-		if (read_size > space_avail) {
-			read_size = space_avail;
-		}
-
-		memcpy(data8,
-		       ((uint8_t*)multiring->ptr_read.storage->ptr) + multiring->ptr_read.offset,
-		       read_size);
-		multiring_advance_read(multiring, read_size);
-
-		len -= read_size;
-		data8 += read_size;
-	}
-}
-
 static inline uint8_t multiring_read_one(multiring_t *multiring) {
 	uint8_t datum;
 
@@ -157,74 +50,6 @@ static inline uint8_t multiring_read_one(multiring_t *multiring) {
 
 static inline void multiring_write_one(multiring_t *multiring, uint8_t datum) {
 	multiring_write(multiring, &datum, 1);
-}
-
-static inline uint16_t multiring_num_wraps(multiring_t *multiring,
-					   uint16_t len) {
-	uint16_t num_wraps = 0;
-	multiring_ptr_t ptr = multiring->ptr_write;
-
-	while (len) {
-		uint16_t chunk_size = len;
-		uint16_t space_avail = multiring_available_contiguous(&ptr);
-
-		if (chunk_size > space_avail) {
-			chunk_size = space_avail;
-		}
-
-		if (chunk_size != len) {
-			num_wraps++;
-		}
-
-		multiring_advance(multiring, &ptr, chunk_size);
-		len -= chunk_size;
-	}
-
-	return num_wraps;
-}
-
-static inline void multiring_memset(multiring_t *multiring, uint8_t val,
-				     uint16_t len) {
-	while (len) {
-		uint16_t write_size = len;
-		uint16_t space_avail =
-			multiring_available_contiguous(&multiring->ptr_write);
-
-		if (write_size > space_avail) {
-			write_size = space_avail;
-		}
-
-		memset(((uint8_t*)multiring->ptr_write.storage->ptr) + multiring->ptr_write.offset,
-		       val, write_size);
-		multiring_advance_write(multiring, write_size);
-
-		len -= write_size;
-	}
-}
-
-static inline uint16_t multiring_byte_delta(multiring_t *multiring,
-					     multiring_ptr_t *first,
-					     multiring_ptr_t *second) {
-	uint16_t len;
-	multiring_ptr_t ptr = *first;
-
-	/* Simple cases: first and second are from the same scatter list entry */
-	if (first->storage == second->storage) {
-		if (first->offset <= second->offset) {
-			return second->offset - first->offset;
-		}
-		return multiring->size - (first->offset - second->offset);
-	}
-
-	/* Complex case: first and second in different scatter list entries */
-	len = first->storage->len - first->offset;
-	do {
-		multiring_next_ring(multiring, &ptr);
-		len += ptr.storage->len;
-	} while (ptr.storage != second->storage);
-	len -= (second->storage->len - second->offset);
-
-	return len;
 }
 
 static inline uint16_t multiring_available(multiring_t *multiring) {
